978ae53ab9d3071eb0dee12c67c754fae3168154
Eric020615 <ericjuncheng10@gmail.com>
2024-09-08 00:36:57 +0800

    fix[camera]: fix camera flash not working

--- src/pages/camera/CameraPage.tsx
+++ src/pages/camera/CameraPage.tsx
@@ -11,12 +11,14 @@ import {
 import * as MediaLibrary from 'expo-media-library'
 import CustomButton from '@components/buttons/CustomButton'
 import Entypo from 'react-native-vector-icons/Entypo'
+import { useApplication } from '@zustand/index'
 
 const CameraPage = () => {
 	const [cameraPermission, requestCameraPermission] = useCameraPermissions()
 	const [image, setImage] = useState<CameraCapturedPicture>(null)
 	const [facing, setFacing] = useState<CameraType>('back')
-	const [flash, setFlash] = useState<FlashMode>('off')
+	const [flash, setFlash] = useState(false)
+	const { setIsLoading } = useApplication()
 	const cameraRef = useRef<CameraView>(null)
 
 	useEffect(() => {
@@ -29,30 +31,39 @@ const CameraPage = () => {
 	const takePicture = async () => {
 		if (cameraRef) {
 			try {
+				setIsLoading(true)
 				const data = await cameraRef.current.takePictureAsync()
 				console.log(data)
 				setImage(data)
 			} catch (error) {
 				console.log(error)
+			} finally {
+				setIsLoading(false)
 			}
 		}
 	}
 
 	if (!cameraPermission) {
-		return <Text>No access to camera</Text>
+		return (
+			<SafeAreaView className="h-full bg-black">
+				<Text className="text-center">No access to camera</Text>
+			</SafeAreaView>
+		)
 	}
 
 	if (!cameraPermission.granted) {
 		return (
-			<View className="flex-1 justify-center">
-				<Text className="text-center pb-10">We need your permission to show the camera</Text>
-				<CustomButton
-					title="Grant Permission"
-					handlePress={requestCameraPermission}
-					containerStyles="border-primary border bg-white p-3 w-full mt-2 flex flex-row self-center"
-					textStyles="text-sm text-primary"
-				/>
-			</View>
+			<SafeAreaView className="h-full bg-black">
+				<View className="flex-1 justify-center mx-8">
+					<Text className="text-center pb-10 text-white">We need your permission to show the camera</Text>
+					<CustomButton
+						title="Grant Permission"
+						handlePress={requestCameraPermission}
+						containerStyles="border-primary border bg-white p-3 w-full mt-2 flex flex-row self-center"
+						textStyles="text-sm text-primary"
+					/>
+				</View>
+			</SafeAreaView>
 		)
 	}
 
@@ -60,8 +71,8 @@ const CameraPage = () => {
 		setFacing((current) => (current === 'back' ? 'front' : 'back'))
 	}
 
-    const toggleFlash = () => {
-		setFlash((current) => (current === 'off' ? 'on' : 'off'))
+	const toggleFlash = () => {
+		setFlash(!flash)
 	}
 
 	const saveImage = async () => {
@@ -79,9 +90,9 @@ const CameraPage = () => {
 	return (
 		<SafeAreaView className="h-full">
 			{!image ? (
-				<CameraView className="flex-1 rounded-3xl" facing={facing} ref={cameraRef}>
+				<CameraView className="flex-1 rounded-3xl" facing={facing} ref={cameraRef} enableTorch={flash}>
 					<View className="flex-1 bg-transparent">
-						<View className='flex-row justify-between px-8 py-4'>
+						<View className="flex-row justify-between px-8 py-4">
 							<CustomButton
 								handlePress={toggleCameraFacing}
 								containerStyles="flex flex-row items-center justify-center h-10"
@@ -92,19 +103,21 @@ const CameraPage = () => {
 								handlePress={toggleFlash}
 								containerStyles="flex flex-row items-center justify-center h-10"
 								textStyles="text-base text-[f1f1f1]] font-bold ml-4"
-								leftReactNativeIcons={<Entypo name="flash" color={flash === 'off' ? 'gray' : '#f1f1f1'} size={28} />}
+								leftReactNativeIcons={
+									<Entypo name="flash" color={flash ? 'gray' : '#f1f1f1'} size={28} />
+								}
 							/>
 						</View>
 					</View>
 				</CameraView>
 			) : (
 				<View className="flex-1">
-					<Image source={{uri: image.uri}} className="flex-1" />
+					<Image source={{ uri: image.uri }} className="flex-1" />
 				</View>
 			)}
 			<View className="bg-black px-5 py-2">
 				{image ? (
-					<View className='flex flex-row justify-between'>
+					<View className="flex flex-row justify-between">
 						<CustomButton
 							title="Re-take"
 							handlePress={() => {
