19c38191a1d73a0b6ff46d0fc849a7b565cfb7e1
Eric020615 <ericjuncheng10@gmail.com>
2024-10-11 14:36:20 +0800

    feat[sub-user]: added sub-user list

--- src/pages/sub-user/SubUserListPage.tsx
+++ src/pages/sub-user/SubUserListPage.tsx
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ListRenderItem, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import CustomModal from '@components/modals/CustomModal'
 import CustomButton from '@components/buttons/CustomButton'
 import { router } from 'expo-router'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+import CustomFlatList from '@components/list/CustomFlatList'
+import { useApplication } from '@store/application/useApplication'
 
 const SubUserListPage = () => {
+	const isLoading = useApplication((state) => state.isLoading)
+	const [page, setPage] = useState(0)
+
+	useEffect(() => {
+		setPage(0)
+	}, [])
+
+	const renderItem: ListRenderItem<any> = ({ item, index }) => (
+		<View className="bg-white p-4 rounded-lg flex flex-row justify-between" key={index}></View>
+	)
+
+	const fetchSubUser = async () => {}
+	const fetchNextPage = async () => {
+		// if (isLoading || notices.length >= totalNotices) return
+		// if (notices.length % 10 !== 0) return
+		// setPage((prev) => prev + 1)
+		// // Logic to fetch the next page
+		// fetchNotice() // Fetch the first page again
+	}
+	const onRefresh = async () => {
+		if (isLoading == true) return
+		// Logic to refresh data
+		setPage(0)
+		// resetNotice()
+		// fetchNotice() // Fetch the first page again
+	}
+
 	return (
 		<SafeAreaView className="bg-slate-100 h-full">
 			<CustomModal />
@@ -29,6 +58,30 @@ const SubUserListPage = () => {
 						/>
 					</View>
 					<Text className="text-3xl text-black font-bold mt-6">Sub-user</Text>
+					<View className="flex-1 mt-4">
+						<CustomFlatList<any>
+							data={[]}
+							renderItem={renderItem}
+							fetchNextPage={fetchNextPage}
+							onRefresh={onRefresh}
+							loading={isLoading}
+							numColumns={1}
+							itemHeight={80} // Customize the item height if needed
+							// listFooterComponent={
+							// 	<View className="py-4 items-center">
+							// 		{isLoading && page > 0 ? (
+							// 			// Show a loading indicator while fetching more data
+							// 			<ActivityIndicator size="large" color="#0000ff" />
+							// 		) : notices.length < totalNotices ? (
+							// 			<Text className="text-gray-500">Load More</Text>
+							// 		) : (
+							// 			// Show a message when all data is loaded
+							// 			<Text className="text-gray-500">You've reached the end of the list.</Text>
+							// 		)}
+							// 	</View>
+							// }
+						/>
+					</View>
 				</View>
 			</View>
 		</SafeAreaView>
